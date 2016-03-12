57c42f14ed1828d397e5cc913ee918ee771affe3
Jiang Sheng <gisonrg@gmail.com>
2016-03-13 03:38:36 +0800

    Implement export json schema. Fixes #11.

--- src/app/app.modalCtrl.js
+++ src/app/app.modalCtrl.js
@@ -3,6 +3,34 @@
 	angular.module('myApp').controller('JsonViewerModalCtrl', function ($scope, $uibModalInstance, data) {
 		$scope.data = data;
 
+		// export json data
+		// copied from http://bgrins.github.io/devtools-snippets/#console-save
+		$scope.export = function () {
+			if (!$scope.data) {
+				return;
+			}
+
+			var data = $scope.data;
+			var filename = 'schema.json';
+
+			if (typeof data === 'object') {
+				data = JSON.stringify(data, null, 2);
+			} else {
+				return;
+			}
+
+			var blob = new Blob([data], {type: 'text/json'}),
+				e = document.createEvent('MouseEvents'),
+				a = document.createElement('a');
+
+			a.download = filename;
+			a.href = window.URL.createObjectURL(blob);
+			a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+			e.initEvent('click', true, false, window,
+				0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			a.dispatchEvent(e);
+		};
+
 		$scope.ok = function () {
 			$uibModalInstance.close();
 		};
