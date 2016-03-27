1089042f869c0cf938d396aa53c64a515c15d367
Jiang Sheng <gisonrg@gmail.com>
2016-03-28 00:11:55 +0800

    separate model of the app

--- src/app/model/model.js
+++ src/app/model/model.js
@@ -1,18 +1,3 @@
-(function (angular) {
-	'use strict';
-	angular.module('myApp.model', []);
-
-	angular.module('myApp.model')
-		.constant('DataType', [
-			new DataType('INTEGER'),
-			new DataType('NUMERIC'),
-			new DataType('VARCHAR', 128),
-			new DataType('TEXT'),
-			new DataType('DATE'),
-			new DataType('TIMESTAMP')
-		]);
-})(window.angular);
-
 /**
  * Models shared globally
  */
